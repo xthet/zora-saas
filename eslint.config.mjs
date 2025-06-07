a24416c4095feaee7c17c645e62f7bdaf16804f5
thet <agnusthet@gmail.com>
2025-06-07 12:08:39 +0100

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		"no-console": "off",
+		"react-hooks/exhaustive-deps": "off",
+		"space-infix-ops": ["warn", { int32Hint: false }],
+		"@typescript-eslint/no-explicit-any": ["off"],
+		"@next/next/no-img-element": ["off"],
+	},
+]
 
-export default eslintConfig;
+export default eslintConfig
